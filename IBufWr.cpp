ff667a44b4743c3474e3e21deaeb3730a8ed1e14
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2018-03-25 14:11:08 +0100

    removed the index looping option, instead clipping it to the buffersize frame count

--- IBufWr.cpp
+++ IBufWr.cpp
@@ -1,5 +1,7 @@
 #include "SC_PlugIn.h"
 
+#define CLIP(a, lo, hi) ( (a)>(lo)?( (a)<(hi)?(a):(hi) ):(lo) )
+
 static InterfaceTable *ft;
 
 static inline bool checkBuffer(Unit * unit, const float * bufData, uint32 bufChannels, uint32 expectedChannels, int inNumSamples) {
@@ -21,28 +23,6 @@ static inline bool checkBuffer(Unit * unit, const float * bufData, uint32 bufCha
   unit->mDone = true;
   ClearUnitOutputs(unit, inNumSamples);
   return false;
-}
-
-
-inline double sc_loop(Unit *unit, double in, double hi, int loop) {
-  // avoid the divide if possible
-  if (in >= hi) {
-    if (!loop) {
-      unit->mDone = true;
-      return hi;
-    }
-    in -= hi;
-    if (in < hi) return in;
-  } else if (in < 0.) {
-    if (!loop) {
-      unit->mDone = true;
-      return 0.;
-    }
-    in += hi;
-    if (in >= 0.) return in;
-  } else return in;
-
-  return in - hi * floor(in/hi);
 }
 
 struct IBufWr : public Unit
@@ -67,29 +47,24 @@ void IBufWr_Ctor(IBufWr *unit)
 
 void IBufWr_next(IBufWr *unit, int inNumSamples)
 {
-  float *phasein  = ZIN(1);
-  uint32 loop     = (uint32)ZIN0(2);
+  float *phasein  = IN(1);// instead of ZIN which was coping with the offset
 
-  GET_BUF //this macro, defined in  SC_Unit.h, does all the sanity check and assigns valutes to bufData, bufChannels, numInputChannels, bufFrames
-  uint32 numInputChannels = unit->mNumInputs - 3;// minus 3 because the arguments are all passed after the input array
+  GET_BUF //this macro, defined in  SC_Unit.h, does all the sanity check, locks the buffer and assigns valutes to bufData, bufChannels, numInputChannels, bufFrames
+  uint32 numInputChannels = unit->mNumInputs - 2;// minus 2 because the arguments are all passed after the input array
 
   // other sanity check, mostly of size
   if (!checkBuffer(unit, bufData, bufChannels, numInputChannels, inNumSamples))
     return;
 
-  //defines the maximum loop bound.
-  double loopMax = (double)(bufFrames - (loop ? 0 : 1));
-
   //iterates through the input samples
   for (uint32 k=0; k<inNumSamples; ++k) {
-    // this unwraps the index should loop be on. then casts it as int32
-    double phase = sc_loop((Unit*)unit, ZXP(phasein), loopMax, loop);
-    int32 iphase = (int32)phase;
+    // this clips the index to the buffer size in frames. then casts it as int32
+    int32 iphase = (int32)CLIP(phasein[k],0,(bufFrames-1));
     // defines a 'table' which is a simple float pointer as a facilitator for pointer arythmetics, using the index (iphase) multipled by the number of channels in the buffer (the data is interleaved)
     float* table0 = bufData + iphase * bufChannels;
     // iterator for the width of the input array/stream
     for (uint32 channel=0; channel<numInputChannels; ++channel)
-      table0[channel] = IN(channel+3)[k];
+      table0[channel] = IN(channel+2)[k];// adds 2 to offset the 2 arguments
   }
 }
 
--- IBufWr.sc
+++ IBufWr.sc
@@ -1,9 +1,9 @@
 IBufWr : UGen {
-	*ar { arg inputArray, bufnum=0, phase=0.0, loop=1.0;
-		^this.multiNewList(['audio', bufnum, phase, loop] ++ inputArray.asArray)
+	*ar { arg inputArray, bufnum=0, phase=0.0;
+		^this.multiNewList(['audio', bufnum, phase] ++ inputArray.asArray)
 	}
-	*kr { arg inputArray, bufnum=0, phase=0.0, loop=1.0;
-		^this.multiNewList(['control', bufnum, phase, loop] ++ inputArray.asArray)
+	*kr { arg inputArray, bufnum=0, phase=0.0;
+		^this.multiNewList(['control', bufnum, phase] ++ inputArray.asArray)
 	}
 	checkInputs {
 		if (rate == 'audio' and: {inputs.at(1).rate != 'audio'}, {
