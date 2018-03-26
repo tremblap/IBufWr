72d7b3cc4b47a27e59ac99b5fbd66d4e6cac3cfa
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2018-03-26 20:45:43 +0100

    spying on the initial values of unit->l_nb_val

--- IBufWr.cpp
+++ IBufWr.cpp
@@ -56,7 +56,10 @@ void IBufWr_Ctor(IBufWr *unit) {
 
   //initialise the other instance variables
   unit->l_index_precedent = -1;
+  unit->l_nb_val = 0;
+
   Print("nb of inputs %i\n", unit->mNumInputs);
+  Print("nb_val %i\n", unit->l_nb_val);
 
   // defines the ugen in the tree
   SETCALC(IBufWr_next);
